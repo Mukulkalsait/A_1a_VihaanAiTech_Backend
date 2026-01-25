85264f809703aedc1ba9e5584817f3e50bd504de
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-26 00:39:53 +0530

    Main rs changed working

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,19 @@
-use axum::{Router, serve::Listener};
-use dotenvy::dotenv;
-use std::net::{SocketAddr, TcpListener};
-use tracing_subscriber;
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+// use dotenvy::dotenv;
+// use std::net::{SocketAddr, TcpListener};
+// use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello Axum 0.8.8 " }));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let addr = "0.0.0.0:3000";
+    let listener = TcpListener::bind(addr).await.expect("Fail to bind address");
+
     println!("🚀 Service Running on http://{}", addr);
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await.expect("Server Failed");
 }
