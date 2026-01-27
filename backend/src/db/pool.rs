df97c48dcf310d0ed3e7fa7b5a73c35b17054820
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-01-28 00:22:48 +0530

    added .env

--- backend/src/db/pool.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::{MySqlPool, mysql::MySqlPoolOptions};
-use crate::config;
-
-pub async fn init_db() -> anyhow::Result<MySqlPool> {
-    let pool = MySqlPoolOptions::new().max_connections(10).connect(&config::database_url()).await?;
-    OK(pool)
-}
