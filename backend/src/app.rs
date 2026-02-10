499736ce5d40b147c748213317893ff9efba42bb
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-10 23:05:23 +0530

    app

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,10 +1,12 @@
 
 use axum::{Router, routing::get};
+use crate::handlers::test;
 
 pub fn build_app()-> Router{
     Router::new()
         .route("/appx", get(appx))
         .route("/health", get(health))
+        .route("/fail",get(test::fail))
 }
 
 async fn health()->&'static str{"ok"}
