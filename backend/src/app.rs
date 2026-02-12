e17607858b193269e69162d8510a08fafeca0080
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-12 23:15:19 +0530

    app rs

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,13 +1,20 @@
 
 use axum::{Router, routing::get};
-use crate::handlers::test;
+use crate::handlers::test::{self, fail};
+use crate::app_state::AppState;
+use crate::config::AppConfig;
 
-pub fn build_app()-> Router{
+
+pub fn build_app(config:AppConfig)-> Router{
+    let state = AppState {config};
     Router::new()
-        .route("/appx", get(appx))
         .route("/health", get(health))
-        .route("/fail",get(test::fail))
+        .with_state(state)
+        .route("/appx", appx())
+
 }
 
 async fn health()->&'static str{"ok"}
 async fn appx()->&'static str {"even this works"}
+
+
