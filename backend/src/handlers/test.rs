eba1219c6581ce6ac20b0d0b754ae18844f055dc
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-02-12 23:15:16 +0530

    test

--- backend/src/handlers/test.rs
+++ backend/src/handlers/test.rs
@@ -1,6 +1,13 @@
 
-use crate::errors::ApiError;
+ use crate::errors::ApiError;
+ use crate::app_state::AppState;
+ use axum::extract::State;
 
-pub async fn fail()-> Result<&'static str, ApiError> {
-    Err(ApiError::Unauthorized)
+ pub async fn fail( State(state): State<AppState>, )-> Result<&'static str, ApiError> {
+     println!("ENV = {}",state.config.app_env);
+     Err(ApiError::Unauthorized)
+ }
+
+pub async fn forbid()-> Result<&'static str, ApiError>{
+    Err(ApiError::Forbiden)
 }
