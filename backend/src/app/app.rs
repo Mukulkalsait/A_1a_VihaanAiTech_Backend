81548e355843ab0c3acd8e77cdfd361fe48a6024
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-07 04:00:38 +0530

    Apps rs updated

--- backend/src/app/app.rs
+++ backend/src/app/app.rs
@@ -1,15 +1,23 @@
+// FILE: ./src/app/app.rs
+
 use axum::{Router, routing};
 
 // EXT
-use crate::app;
 use crate::app::AppState;
 use crate::config;
 use crate::handlers::{create_user, google_auth, list_user, me, test};
 
-//ENT
+// INT
 
+/// IMP: Router::new()...route().with_state(state) => tells axum that
+/// Every request can access =>
+///     - State(state): State<AppState> directly anywhere.
+///     - even inside functions parameters.
+/// THEREFORE=>
+///     - DB from this state can be passed into function.
+///
 pub fn build_app(config: config::AppConfig, db: sqlx::SqlitePool) -> axum::Router {
-    let state = AppState { config, db };
+    let state = AppState { config, db }; // cofig + db.pull 
     Router::new()
         .route("/home", routing::get(homepageurl))
         // important
@@ -24,7 +32,7 @@ pub fn build_app(config: config::AppConfig, db: sqlx::SqlitePool) -> axum::Route
 }
 
 async fn homepageurl() -> &'static str {
-    "ok"
+    "<html><h1>Welcome to homepage</h1></html>"
 }
 async fn appx() -> &'static str {
     "appx"
