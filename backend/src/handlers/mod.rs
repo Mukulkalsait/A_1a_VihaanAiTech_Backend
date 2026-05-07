3d09793186877f0c4c5e47b84ae3de2472c992b6
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-07 16:48:27 +0530

    Mod mobile

--- backend/src/handlers/mod.rs
+++ backend/src/handlers/mod.rs
@@ -2,10 +2,12 @@
 
 pub mod google_auth;
 pub mod me_auth;
+pub mod mobile_auth;
 pub mod test;
 pub mod user;
 
 // Re-export
 pub use me_auth::me;
+pub use mobile_auth::{mobile_login, mobile_register};
 pub use user::create_user;
 pub use user::list_user;
