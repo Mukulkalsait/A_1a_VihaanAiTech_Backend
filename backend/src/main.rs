9347e9a48fab9e5f2e3f45a3687e05c4e9242df8
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-07 16:49:06 +0530

    Main Updated changed the db createion while compilation

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,5 @@
+// FILE: /src/main.rs
+
 mod app;
 mod config;
 // use crate::config::env::AppConfig;
@@ -13,18 +15,17 @@ mod utils;
 /// resualt -> <Ok(), Err()> | But for generic type => (error? if yes type?)
 /// anyhow allow us to use ? early return. => give anyting to return as resualt.
 async fn main() -> anyhow::Result<()> {
-    let config = config::AppConfig::from_env()?;
-    let db = sqlx::sqlite::SqlitePoolOptions::new().max_connections(6).connect(&config.database_url).await?;
+    let config = config::AppConfig::from_env()?; // dot env file
+    let db = sqlx::sqlite::SqlitePoolOptions::new().max_connections(6).connect(&config.database_url).await?; // ACTUAL POOL CREATION WITH 6 connections.
     let app = app::build_app(config.clone(), db.clone());
 
-    db::init::init_db(&db).await?;
+    // db::init::init_db(&db).await?; // create db... R: STOPED can removed
 
     let listener = tokio::net::TcpListener::bind(&config.server_addr).await?;
     // await can ONLY used on something that might take time. eg. Network, socket, timers, file I/O
     // listner say 'i need this port 👆', OS can say => {delay, fail, wait for other resources} ➡️ await.
     // await suspends the current async task until the awaited future makes progress
-    // Y: await
-    // G: So the function is paused till OS binds the port.
+    // Y: await : So the function is paused till OS binds the port.
 
     println!("⚡ VAT on {}", &config.server_addr);
     axum::serve(listener, app.into_make_service()).await?;
@@ -36,6 +37,6 @@ async fn main() -> anyhow::Result<()> {
 // 1. AppConfig (config/env) | envFileDetails.
 // 2. AppState (config + db)
 // 3. ApiError (Errors )
-// 4. /db/init db_init() | db_query  sqlx::query().exicute(pool).await?
+// 4. /db/init db_init() =>  db_query  + pool => sqlx::query().exicute(pool).await?
 // 5. GoogleAuth
 // 6. App::build_app () -> Router |  Router::new .... (AppState)
