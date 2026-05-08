0420a4a59e5ec6808cbdafefb2791be90643b0f1
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-08 16:53:35 +0530

    Updates

--- shell.nix
+++ shell.nix
@@ -1,17 +1,39 @@
 { pkgs ? import <nixpkgs> { } }:
 
+let
+  # NEW VERSION PER BUILD ✨
+  # fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") { };
+  # rustToolchain = fenix.complete.toolchain;
+
+
+  # PINNED VERSION: ✨
+  fenix = import
+    (fetchTarball {
+      url = "https://github.com/nix-community/fenix/archive/main.tar.gz";
+      sha256 = "sha256-xxxxxxxxxxxxxxxx";
+    })
+    { };
+
+in
+
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.openssl
-    pkgs.pkg-config
-    pkgs.clang
-    pkgs.llvmPackages.lld
-    pkgs.binutils
+  buildInputs = with pkgs; [
+    rustToolchain
+
+    openssl
+    pkg-config
+    clang
+    llvmPackages.lld
+    # binutils # only when debuging linker Issues.
+
+    sqlite
+    sqlx-cli
   ];
 
   OPENSSL_NO_VENDOR = 1;
 
   shellHook = ''
+
     export HISTFILE="$PWD/.zsh_history"
     export HISTSIZE=10000
     export SAVEHIST=10000
@@ -30,5 +52,11 @@ pkgs.mkShell {
       zsh
       exit
     fi
+
+
+
+    echo "🦀 Rust backend dev shell"
+    rustc --version
+    cargo --version
   '';
 }
