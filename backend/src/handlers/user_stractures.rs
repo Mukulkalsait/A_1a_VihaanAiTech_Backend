b0110b152cd562a676e82d8d3c3e19ac07b84e1b
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-10 05:20:31 +0530

    Strcuts are seperated for suers and db.

---
+++ backend/src/handlers/user_stractures.rs
@@ -0,0 +1,19 @@
+#[derive(serde::Deserialize)]
+/// #### Googel Login Workflow
+///
+/// - Frontend => Google Login Popup
+/// - Google returns ID Token => Rrontend sends token to backend => POST /auth/google
+/// - Backend verifies token with Google => Backend finds/creates user
+/// - Backend creates JWT => JWT returned to frontend => Frontend stores JWT
+/// - Future requests send JWT
+pub struct GoogleAuthRequest {
+    pub token: String,
+}
+
+#[derive(sqlx::FromRow)]
+pub struct ExcitingUser {
+    pub id: i64,
+    pub user_email: String,
+    pub user_first_name: String,
+    pub user_picture: Option<String>,
+}
